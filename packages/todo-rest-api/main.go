3cc459da2d62a77113c5e2082218cac6638b2776
Harman Goei <5001432+hrgui@users.noreply.github.com>
2022-07-10 14:49:21 -0700

    chore: now i can mount the go api and hot reload it

--- packages/todo-rest-api/main.go
+++ packages/todo-rest-api/main.go
@@ -3,7 +3,7 @@ package main
 import "github.com/gofiber/fiber/v2"
 
 func listTodos(c *fiber.Ctx) error {
-	return c.SendString("Hello, World!")
+	return c.SendString("Hello, Worldo!")
 }
 
 func getTodoById(c *fiber.Ctx) error {
