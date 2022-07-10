feaaa3cbe93971f726102eb28f923a502199215d
Harman Goei <5001432+hrgui@users.noreply.github.com>
2022-07-10 15:51:49 -0700

    feat: rejoice that listing todos work

--- packages/todo-rest-api/main.go
+++ packages/todo-rest-api/main.go
@@ -1,9 +1,42 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"database/sql"
+	"log"
 
-func listTodos(c *fiber.Ctx) error {
-	return c.SendString("Hello, Worldo!")
+	"github.com/gofiber/fiber/v2"
+	_ "github.com/lib/pq"
+)
+
+type Todo struct {
+	Id int
+	Title string
+	Completed bool
+
+}
+
+
+func listTodos(c *fiber.Ctx, db *sql.DB) error {
+	var todos []Todo
+	// return c.SendString("Hello, Worldo!")
+	// query the db
+	rows, err := db.Query("SELECT id, title, completed FROM todos ORDER BY id ASC")
+	// close the rows, prevent further enumeration
+	defer rows.Close()
+
+	// check if there are errors
+	if err != nil {
+		log.Fatalln(err)
+		return c.JSON("An error occurred")
+	}
+
+	for rows.Next() {
+		todo := Todo{}
+		rows.Scan(&todo.Id, &todo.Title, &todo.Completed)
+		todos = append(todos, todo)
+	}
+
+	return c.JSON(todos)
 }
 
 func getTodoById(c *fiber.Ctx) error {
@@ -24,9 +57,19 @@ func deleteTodo(c *fiber.Ctx) error {
 
 
 func main() {
+	// TODO: environment variables
+	// TODO: can we make this a singleton?
+	connStr := "postgresql://user:postgrespassword@postgres:5432/todos?sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	app := fiber.New()
 
-	app.Get("/todos", listTodos)
+	app.Get("/todos", func (c *fiber.Ctx) error {
+		return listTodos(c, db)
+	})
 	app.Get("/todo/:id", getTodoById)
 	app.Post("/todo", createTodo)
 	app.Put("/todo/:id", updateTodo)
